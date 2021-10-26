fc78d1d3e4743427f0b0fbdfe3137b03f3d03a93
Theo Leão Larrieux <larrieuxtheo@gmail.com>
2021-10-26 15:59:54 -0500

    Optimization implemented

--- Prime.py
+++ Prime.py
@@ -1,27 +1,24 @@
-primeNums = []
-invalidInput = True
-primeNums.append(0)
-while invalidInput:
+alist = []
+check = True
+while check:
     try:
         maxRange = int(input('Max number to find primes: '))
-        invalidInput = False
+        check = False
     except:
         print('Type a valid number', '\n')
 
 for i in range(0, maxRange + 1):
-    
-    for c in primeNums: # For every prime number found
-        if c != 0 and c != 1 and (i % c) == 0: # Check if the number in question (i) is prime, by 
-            break                              # dividing it by every other prime number
-    else:                                      # TODO: Optimize this by checking only the sqrt(primeNums)
-        if len(primeNums) % 100 == 0:
-            print('List size =',len(primeNums), '--- Current number =',primeNums[-1]) # Progress update to the user
-        primeNums.append(i)
 
-primeNums.pop(0)
-primeNums.pop(0)
-primeNums.pop(0)
-primeNums.pop(2)
+    for c in range(int(len(alist) ** 0.5)): # For every prime number found
+        if alist[c] != 0 and alist[c] != 1 and (i % alist[c]) == 0: # Check if the number in question (i) is prime, by 
+            break                                                   # dividing it by every other prime number
+    else:
+        if len(alist) % 100 == 0 and len(alist) != 0:
+            print('List size =', len(alist), '--- Current number =', alist[-1]) # Progress update to the user
+        if i != 0 and i != 1:
+            alist.append(i)
 
-myFile = open('primeOutput.txt','w')
-myFile.write(str(primeNums))
+
+## print(alist)
+myFile = open('primeOutput.txt', 'w')
+myFile.write(str(alist))
